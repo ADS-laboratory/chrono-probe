d20a9e9aba0884cce6f970f53d42b217ac73042a
Leonardo Danelutti <leodane2002@gmail.com>
2022-12-17 11:54:41 +0100

    Added time measurement.

--- src/measurements.rs
+++ src/measurements.rs
@@ -12,16 +12,40 @@ pub fn get_resolution() -> Duration {
     }
 }
 
-pub fn get_times<F>(f: F, strings: Vec<String>) -> Vec<Duration>
+fn get_time_with_resolution<F>(f: F, string: String, relative_error: f32, resolution: Duration) -> Duration
+    where
+        F: Fn(&[u8]) -> (),
+{
+    let mut n = 0;
+    let start = Instant::now();
+    loop {
+        f(string.as_bytes());
+        n += 1;
+        let end = start.elapsed();
+        if end > resolution * ((1.0 / relative_error) + 1.0) as u32 {
+            return end / n;
+        }
+    }
+}
+
+fn get_time<F>(f: F, string: String, relative_error: f32) -> Duration
+    where
+        F: Fn(&[u8]) -> (),
+{
+    let resolution = get_resolution();
+    get_time_with_resolution(f, string, relative_error, resolution)
+}
+
+pub fn get_times<F>(f: F, strings: Vec<String>, relative_error: f32) -> Vec<Duration>
     where
         F: Fn(&[u8]) -> (),
 {
     let n = strings.len();
     let mut times = Vec::with_capacity(n);
+    let resolution = get_resolution();
     for string in strings {
-        let start = Instant::now();
-        f(string.as_bytes());
-        times.push(start.elapsed());
+        let time = get_time_with_resolution(&f, string, relative_error, resolution);
+        times.push(time);
     }
     times
 }
