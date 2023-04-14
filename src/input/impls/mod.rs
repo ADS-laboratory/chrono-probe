b8d0ecf2dabe0967859640d12d46ee53b2e2a20f
Lorenzo Della Giustina <dellagiustina.lorenzo@gmail.com>
2023-04-14 14:12:39 +0000

    Improved  macro implementation

--- src/input/impls/mod.rs
+++ src/input/impls/mod.rs
@@ -2,50 +2,59 @@ pub mod distribution;
 
 /// Implements Input for the given type using the given closure to get the size of the input.
 /// Useful for not having to create a wrapper for built-in data types.
-///
+/// 
+/// # Syntax
+/// 
+/// (`$generate_input_closure`)(`$builder`) -> `$input`, `$get_size_closure`
+/// 
 /// # Arguments
-///
-/// * `$built_in_type` - The type to implement [Input](crate::input::Input) for.
-/// * `$closure` - The closure that will be used to get the size of the input.
-///
+/// 
+/// * `$generate_input_closure` - The closure that will be used to generate the input through the builder.
+///     (`|usize, $builder| -> $built_in_type`)
+/// * `$builder` - The type of the builder that will be used to generate the input.
+/// * `$input` - The type to implement [Input](crate::input::Input) for.
+/// * `$get_size_closure` - The closure that will be used to get the size of the input.
+///     (`|$built_in_type| -> usize`)
+/// 
 /// # Example
-///
+/// 
 /// ```
 /// impl_input!(Vec<i32>, |v: Vec<i32>| v.len());
 /// ```
 #[macro_export]
 macro_rules! impl_input {
-    ($built_in_type:ty, $closure:expr) => {
+    (($generate_input_closure:expr)($builder:ty) -> $input:ty, $get_size_closure:expr) => {
         /// Implementation of "Input" for $built_in_type
-        impl crate::input::Input for $built_in_type {
-            type Builder = fn(usize) -> Self;
+        impl crate::input::Input for $input {
+            type Builder<'a> = $builder;
 
             /// Gets the size of the input.
             fn get_size(&self) -> usize {
-                $closure(self.clone())
+                $get_size_closure(self.clone())
             }
 
             /// Generates an input of the given size using the given builder.
-            ///
+            /// 
             /// # Arguments
-            ///
+            /// 
             /// * `size` - The size of the input to be generated.
             /// * `builder` - The builder that will be used to generate the input (it's basically a function).
-            ///
+            /// 
             /// # Example
-            ///
+            /// 
             /// ```
             /// use time_complexity_plot::input::Input;
-            ///
-            /// fn builder(size: usize) -> Vec<i32> {
-            ///    vec![0; size]
+            /// 
+            /// struct VecBuilder<'a> {
+            ///     func: &'a fn(usize) -> Vec<i32>,
             /// }
-            ///
-            /// impl_input!(Vec<i32>, |v: Vec<i32>| v.len());
+            /// 
+            /// impl_input!( (|size: usize, builder: VecBuilder| (builder.func)(size))(VecBuilder<'a>) -> Vec<i32>, |v: Vec<i32>| v.len());
+            /// let builder: VecBuilder = todo!();
             /// let input = Vec::<i32>::generate_input(10, builder);
             /// ```
             fn generate_input(size: usize, builder: Self::Builder) -> Self {
-                builder(size)
+                $generate_input_closure(size, builder)
             }
         }
     };
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![warn(clippy::all)]
 #![warn(clippy::cargo)]
-#![feature(associated_type_defaults)] // nightly only
 pub mod input;
 pub mod measurements;
 pub mod plot;
